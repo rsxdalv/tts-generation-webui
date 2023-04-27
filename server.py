4131f6a04a6afff978e4cab940e310f648ed0753
Roberts Slisans <rsxdalv@gmail.com>
2023-04-27 22:08:30 +0300

    history prompts

--- server.py
+++ server.py
@@ -20,12 +20,23 @@ def init_models():
 
 init_models()
 
-# generate audio from text
-text_prompt = """
-     Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
-     But I also have other interests such as playing tic tac toe.
-"""
-audio_array = generate_audio(text_prompt)
+def generate(prompt, noHistory, history_prompt):
+    # generate audio from text
+    history_prompt = None if noHistory else history_prompt
 
-# To save audio_array as a WAV file:
-write_wav("outputs/audio.wav", SAMPLE_RATE, audio_array)
+    audio_array = generate_audio(prompt, history_prompt=history_prompt)
+
+
+    # To save audio_array as a WAV file:
+    filename = f"outputs/audio_{history_prompt}.wav"
+    write_wav(filename, SAMPLE_RATE, audio_array)
+
+text_prompt = """
+    Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
+    But I also have other interests such as playing tic tac toe.
+"""
+
+history_prompt = "en_speaker_0"
+generate(text_prompt, False, history_prompt)
+generate(text_prompt, True, history_prompt)
+
