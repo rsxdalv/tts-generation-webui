61e2ab0ed8bc2a2586a45f946a2aa9a15d1b8682
Roberts Slisans <rsxdalv@gmail.com>
2023-05-02 02:32:53 +0300

    Enable v2 history_prompts

--- create_base_filename.py
+++ create_base_filename.py
@@ -2,4 +2,7 @@ import os
 
 
 def create_base_filename(title, output_path, model, date):
-    return os.path.join(output_path, f"audio__{model}__{title}__{date}")
+    return os.path.join(output_path, f"audio__{model}__{replace_path_sep(title)}__{date}")
+
+def replace_path_sep(title):
+    return title.replace(os.path.sep, '_')
