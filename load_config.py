3185761c2b092933667bbb536c6980fcc9ba0036
Roberts Slisans <rsxdalv@gmail.com>
2023-05-02 19:16:38 +0300

    improve config recovery architecture

--- load_config.py
+++ load_config.py
@@ -1,6 +1,26 @@
 import json
+import os
 
+default_config = {
+    "model": {
+        "text_use_gpu": True,
+        "text_use_small": True,
+        "coarse_use_gpu": True,
+        "coarse_use_small": True,
+        "fine_use_gpu": True,
+        "fine_use_small": False,
+        "codec_use_gpu": True,
+        "load_models_on_startup": True
+    },
+    "load_models_on_startup": True
+}
 
 def load_config():
-    with open('config.json', 'r') as infile:
-        return json.load(infile)
+    if os.path.exists('config.json'):
+        with open('config.json', 'r') as infile:
+            return json.load(infile)
+    else:
+        print("Config file not found. Creating default config.")
+        with open('config.json', 'w') as outfile:
+            json.dump(default_config, outfile, indent=2)
+            return default_config
--- setup_or_recover.py
+++ setup_or_recover.py
@@ -1,26 +1,9 @@
-import json
 import os
 
 def dummy():
     pass
 
 def setup_or_recover():
-    if not os.path.exists('config.json'):
-        print("Config file not found. Creating default config.")
-        with open('config.json', 'w') as outfile:
-            json.dump({
-                "model": {
-                    "text_use_gpu": True,
-                    "text_use_small": True,
-                    "coarse_use_gpu": True,
-                    "coarse_use_small": True,
-                    "fine_use_gpu": True,
-                    "fine_use_small": False,
-                    "codec_use_gpu": True,
-                    "load_models_on_startup": True
-                },
-                "load_models_on_startup": True
-            }, outfile, indent=2)
     if not os.path.exists('outputs'):
         os.makedirs('outputs')
     if not os.path.exists('favorites'):
