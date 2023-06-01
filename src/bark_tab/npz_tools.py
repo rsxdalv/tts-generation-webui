b4ad1744d51edf3b96045dd89d5f450708a332d1
Roberts Slisans <rsxdalv@gmail.com>
2023-06-01 09:39:53 +0300

    compressed history

--- src/bark_tab/npz_tools.py
+++ src/bark_tab/npz_tools.py
@@ -1,12 +1,20 @@
 import glob
 import numpy as np
 
+from .FullGeneration import FullGeneration
+
 
 def npz_tools():
     return None
 
+def compress_history(full_generation):
+    full_generation["semantic_prompt"] = full_generation["semantic_prompt"].astype(np.int16)
+    full_generation["coarse_prompt"] = full_generation["coarse_prompt"].astype(np.int16)
+    full_generation["fine_prompt"] = full_generation["fine_prompt"].astype(np.int16)
+    return full_generation
 
-def save_npz(filename, full_generation):
+def save_npz(filename, full_generation: FullGeneration):
+    full_generation = compress_history(full_generation)
     np.savez(filename, **full_generation)
 
 
