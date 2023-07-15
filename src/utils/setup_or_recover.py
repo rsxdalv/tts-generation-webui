44bf7ef1719350399e0df7439444ac6f62af5e92
Roberts Slisans <rsxdalv@gmail.com>
2023-07-15 18:48:36 +0300

    improve env generator

--- src/utils/setup_or_recover.py
+++ src/utils/setup_or_recover.py
@@ -5,19 +5,31 @@ def dummy():
     pass
 
 
+def env_entry(name, value, comment):
+    return f"# {comment}\n{name}={value}\n"
+
+
 def generate_env(
     environment_suno_use_small_models: bool,
     environment_suno_enable_mps: bool,
     environment_suno_offload_cpu: bool,
 ):
-    return f"""
-# Duplicates small models checkboxes
-SUNO_USE_SMALL_MODELS={environment_suno_use_small_models}
-# Use MPS when CUDA is unavailable
-SUNO_ENABLE_MPS={environment_suno_enable_mps}
-# Offload GPU models to CPU
-SUNO_OFFLOAD_CPU={environment_suno_offload_cpu}
-"""
+    env = "# This file gets updated automatically from the UI"
+    env += env_entry(
+        "SUNO_USE_SMALL_MODELS",
+        environment_suno_use_small_models,
+        "Duplicates small models checkboxes",
+    )
+    env += env_entry(
+        "SUNO_ENABLE_MPS",
+        environment_suno_enable_mps,
+        "Use MPS when CUDA is unavailable",
+    )
+    env += env_entry(
+        "SUNO_OFFLOAD_CPU", environment_suno_offload_cpu, "Offload GPU models to CPU"
+    )
+
+    return env
 
 
 def setup_or_recover():
