1917c0fb00d4fbaebd58d6939ed7af547da60426
Roberts Slisans <rsxdalv@gmail.com>
2023-07-16 11:27:07 +0300

    add open buttons for folders

--- src/tortoise/gr_reload_button.py
+++ src/tortoise/gr_reload_button.py
@@ -1,10 +1,25 @@
 import gradio as gr
+from src.history_tab.open_folder import open_folder
 
 
-def gr_reload_button(**kwargs):
+def gr_icon_button(value="refresh", **kwargs):
     return gr.Button(
-        "refresh",
+        value=value,
         elem_classes="btn-sm material-symbols-outlined",
         size="sm",
         **kwargs,
+    )
+
+
+def gr_reload_button(**kwargs):
+    return gr_icon_button(value="refresh", **kwargs)
+
+
+def gr_open_button(**kwargs):
+    return gr_icon_button(value="folder_open", **kwargs)
+
+
+def gr_open_button_simple(dirname="", **kwargs):
+    return gr_open_button(**kwargs).click(
+        fn=lambda: open_folder(dirname),
     )
