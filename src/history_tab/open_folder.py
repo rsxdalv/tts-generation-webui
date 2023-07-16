ad8cf462f52a92c35c38806ae4771540a1f8f719
Roberts Slisans <rsxdalv@gmail.com>
2023-07-16 11:26:46 +0300

    improved open_folder

--- src/history_tab/open_folder.py
+++ src/history_tab/open_folder.py
@@ -1,5 +1,24 @@
-import os
+import subprocess
+import sys
+
+if sys.platform == "darwin":
+
+    def open_folder(folder_path: str):
+        subprocess.check_call(["open", "--", folder_path])
+
+elif sys.platform == "linux2":
+
+    def open_folder(folder_path: str):
+        subprocess.check_call(["xdg-open", "--", folder_path])
+
+elif sys.platform == "win32":
+
+    def open_folder(folder_path: str):
+        subprocess.Popen(["explorer", folder_path])
 
 
-def open_folder(folder_path: str):
-    os.startfile(folder_path)
+if __name__ == "__main__":
+    # open_folder("./data/models/")
+    import os
+
+    open_folder(os.path.join(os.path.dirname(__file__), "..", "data", "models"))
