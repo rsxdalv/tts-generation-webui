1ea71f7367ac0f3b379b489ccac36d63df32f41f
Roberts Slisans <rsxdalv@gmail.com>
2024-01-05 17:46:09 +0200

    add index page

--- react-ui/src/pages/index.tsx
+++ react-ui/src/pages/index.tsx
@@ -13,6 +13,21 @@ export default function Home({ voices }: { voices: Voice[] }) {
       <Head>
         <title>TTS Generation Webui</title>
       </Head>
+      <div className="text-center space-y-4 max-w-2xl mx-auto mb-auto">
+        <h1 className="text-3xl font-bold">Welcome to the TTS Webui!</h1>
+        <p className="text-xl">
+          This is a web interface for the TTS project. It allows you to generate
+          audio using the TTS models.
+        </p>
+        <p className="text-xl">
+          To get started, select a tab above to choose a model and generate some
+          audio.
+        </p>
+        <p className="text-xl">
+          You can also listen to some of the voices that have been generated
+          using the "Favorites" button.
+        </p>
+      </div>
     </Template>
   );
 }
