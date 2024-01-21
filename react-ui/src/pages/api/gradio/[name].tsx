d1aec3f7d2a5bf4a1c990abc7a5c16fcc266483b
Roberts Slisans <rsxdalv@gmail.com>
2024-01-21 14:40:30 +0200

    change react backend API to use an environment variable

--- react-ui/src/pages/api/gradio/[name].tsx
+++ react-ui/src/pages/api/gradio/[name].tsx
@@ -2,11 +2,10 @@ import { client } from "@gradio/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getFile } from "../../../backend-utils/getFile";
 import { GradioFile } from "../../../types/GradioFile";
-import { join } from "path";
 
 type Data = { data: any };
 
-const defaultBackend = "http://127.0.0.1:7860/";
+const defaultBackend = process.env.GRADIO_BACKEND || "http://127.0.0.1:7860/";
 const getClient = () => client(defaultBackend, {});
 
 export default async function handler(
@@ -221,7 +220,7 @@ async function reload_old_generation_dropdown() {
     ];
   };
 
-  return result?.data[0].choices.map(x => x[0]);
+  return result?.data[0].choices.map((x) => x[0]);
 }
 
 async function bark_favorite({ history_bundle_name_data }) {
@@ -315,7 +314,7 @@ async function tortoise_refresh_models() {
     ];
   };
 
-  return result?.data[0].choices.map(x => x[0])
+  return result?.data[0].choices.map((x) => x[0]);
 }
 
 async function tortoise_refresh_voices() {
@@ -330,7 +329,7 @@ async function tortoise_refresh_voices() {
     ];
   };
 
-  return result?.data[0].choices.map(x => x[0])
+  return result?.data[0].choices.map((x) => x[0]);
 }
 
 async function tortoise_open_models() {
@@ -440,7 +439,7 @@ async function rvc_model_reload() {
     ];
   };
 
-  return result?.data[0].choices.map(x => x[0]);
+  return result?.data[0].choices.map((x) => x[0]);
 }
 
 async function rvc_index_reload() {
@@ -455,7 +454,7 @@ async function rvc_index_reload() {
     ];
   };
 
-  return result?.data[0].choices.map(x => x[0]);
+  return result?.data[0].choices.map((x) => x[0]);
 }
 
 // rvc_model_open
@@ -672,7 +671,7 @@ async function magnet_get_models() {
     ];
   };
 
-  return result?.data[0].choices.map(x => x[0]);
+  return result?.data[0].choices.map((x) => x[0]);
 }
 
 // magnet_open_model_dir
