5f4f2c75ec6f97883d1bf821ac744c54e6c3f88a
Roberts Slisans <rsxdalv@gmail.com>
2024-05-06 12:36:24 +0300

    improve link organization and linkbar format

--- react-ui/src/components/Template.tsx
+++ react-ui/src/components/Template.tsx
@@ -9,7 +9,7 @@ export const Template = ({ children }: { children: React.ReactNode }) => (
     <LocalVotesProvider>
       <VotesProvider>
         <main
-          className={`flex min-h-screen flex-col items-center justify-start p-12 gap-y-2 ${inter.className}`}
+          className={`flex min-h-screen flex-col items-center justify-start px-8 py-6 gap-y-2 ${inter.className}`}
         >
           <Header />
           {children}
