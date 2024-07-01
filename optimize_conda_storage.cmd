d2b0a4151326c250c9b3f65b7587cbd289cda38a
Roberts Slisans <rsxdalv@gmail.com>
2024-07-01 20:36:47 +0300

    improve conda storage clean script

--- optimize_conda_storage.cmd
+++ optimize_conda_storage.cmd
@@ -1,2 +1,8 @@
-@REM conda clean --all --dry-run
+@REM invoke cmd_windows.bat
+call ../cmd_windows.bat
+
+echo "Dry run"
+conda clean --all --dry-run
+pause
+echo "Confirm cleaning files or cancel"
 conda clean --all
