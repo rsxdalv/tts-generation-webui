3eb430aca789f7e50040881ab3772f12fbf3f7cc
Roberts Slisans <rsxdalv@gmail.com>
2024-07-06 17:56:01 +0300

    add conda cmd/bash utility scripts

---
+++ installer_scripts/conda_env_cmd.bat
@@ -0,0 +1,15 @@
+@echo off
+
+cd ..
+call .\installer_files\mamba\condabin\activate.bat .\installer_files\env
+
+
+echo Starting command prompt for user to run commands in
+
+call conda --version
+echo Node
+call node --version
+call python --version
+
+cmd /k "%*"
+
