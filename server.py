77789d73804296ac492020d8a29cc33c9d258e9a
Roberts Slisans <rsxdalv@gmail.com>
2024-07-13 20:51:09 +0300

    extract suppress warnings

--- server.py
+++ server.py
@@ -2,44 +2,9 @@ import os
 import src.utils.setup_or_recover as setup_or_recover
 import src.utils.dotenv_init as dotenv_init
 import gradio as gr
-import warnings
+from utils.suppress_warnings import suppress_warnings
 
-warnings.filterwarnings(
-    "ignore",
-    message="Using the update method is deprecated. Simply return a new object instead",
-)
-warnings.filterwarnings(
-    "ignore",
-    message="Trying to convert audio automatically from float32 to 16-bit int format.",
-)
-warnings.filterwarnings(
-    "ignore",
-    message="Trying to convert audio automatically from int32 to 16-bit int format.",
-)
-# UserWarning: torch.utils._pytree._register_pytree_node is deprecated. Please use torch.utils._pytree.register_pytree_node instead.
-warnings.filterwarnings(
-    "ignore",
-    message="torch.utils._pytree._register_pytree_node is deprecated. Please use torch.utils._pytree.register_pytree_node instead.",
-)
-# FutureWarning: transformers.deepspeed module is deprecated and will be removed in a future version. Please import deepspeed modules directly from transformers.integrations
-warnings.filterwarnings(
-    "ignore",
-    message="transformers.deepspeed module is deprecated and will be removed in a future version. Please import deepspeed modules directly from transformers.integrations",
-)
-# UserWarning: torch.nn.utils.weight_norm is deprecated in favor of torch.nn.utils.parametrizations.weight_norm.
-# warnings.warn("torch.nn.utils.weight_norm is deprecated in favor of torch.nn.utils.parametrizations.weight_norm.")
-warnings.filterwarnings(
-    "ignore",
-    message="torch.nn.utils.weight_norm is deprecated in favor of torch.nn.utils.parametrizations.weight_norm.",
-)
-
-import logging
-
-# suppress warning from logging "A matching Triton is not available, some optimizations will not be enabled"
-# suppress warning from logging "Triton is not available, some optimizations will not be enabled."
-logging.getLogger("xformers").addFilter(
-    lambda record: "Triton is not available" not in record.getMessage()
-)
+suppress_warnings()
 
 from src.config.load_config import default_config
 from src.config.config import config
@@ -83,7 +48,7 @@ with gr.Blocks(
 ### _(Text To Speech, Audio & Music Generation, Conversion)_
 """
     )
-    with Joutai.singleton.tabs:
+    with gr.Tabs():
         with gr.Tab("Text-to-Speech"), gr.Tabs():
             from src.bark.generation_tab_bark import generation_tab_bark
 
@@ -101,6 +66,7 @@ with gr.Blocks(
                 print(e)
 
             from src.tortoise.generation_tab_tortoise import generation_tab_tortoise
+
             generation_tab_tortoise()
 
             try:
@@ -248,19 +214,23 @@ with gr.Blocks(
                 print("Failed to load demucs demo")
                 print(e)
 
-            
             from src.vocos.vocos_tabs import vocos_tabs
+
             vocos_tabs()
 
         with gr.Tab("Outputs"), gr.Tabs():
             from src.history_tab.main import history_tab
+
             collections_directories_atom.render()
             history_tab(register_use_as_history_button)
             history_tab(register_use_as_history_button, directory="favorites")
             history_tab(
-                register_use_as_history_button, directory="outputs", show_collections=True
+                register_use_as_history_button,
+                directory="outputs",
+                show_collections=True,
             )
             from src.history_tab.voices_tab import voices_tab
+
             voices_tab(register_use_as_history_button)
 
         with gr.Tab("Settings"), gr.Tabs():
@@ -280,7 +250,6 @@ with gr.Blocks(
 
         # from src.studio.studio_tab import simple_remixer_tab
         # remixer_input = simple_remixer_tab()
-    Joutai.singleton.tabs.render()
 
 
 def print_pretty_options(options):
