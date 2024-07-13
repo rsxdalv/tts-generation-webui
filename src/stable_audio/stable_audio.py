5857d8644c696213e54ee911a6dede3aed98617f
Roberts Slisans <rsxdalv@gmail.com>
2024-07-13 18:13:24 +0300

    improve stable audio filenames with newlines

--- src/stable_audio/stable_audio.py
+++ src/stable_audio/stable_audio.py
@@ -270,6 +270,8 @@ def save_result(audio, *generation_args):
             .replace(")", "_")
             .replace("?", "_")
             .replace("!", "_")
+            .replace("/", "_")
+            .replace("\n", "_")
             # only first 15 characters
             .replace("__", "_")[:15]
         )
