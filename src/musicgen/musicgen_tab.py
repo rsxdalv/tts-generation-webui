4e020926bad891376539957ca463acd3d4b802ac
Roberts Slisans <rsxdalv@gmail.com>
2024-07-15 21:07:57 +0300

    musicgen remove send to demucs

--- src/musicgen/musicgen_tab.py
+++ src/musicgen/musicgen_tab.py
@@ -7,7 +7,6 @@ from einops import rearrange
 from typing import Optional, Tuple, TypedDict
 import numpy as np
 import os
-from src.Joutai import Joutai
 from src.bark.npz_tools import save_npz_musicgen
 from src.musicgen.setup_seed_ui_musicgen import setup_seed_ui_musicgen
 from src.bark.parse_or_set_seed import parse_or_set_seed
@@ -29,6 +28,7 @@ from typing import Optional
 from importlib.metadata import version
 
 AUDIOCRAFT_VERSION = version("audiocraft")
+
 
 class MusicGenGeneration(TypedDict):
     model: str
@@ -149,8 +149,6 @@ def generate(params: MusicGenGeneration, melody_in: Optional[Tuple[int, np.ndarr
     if MODEL is None or MODEL.name != model:
         MODEL = load_model(model)
 
-    # if duration > MODEL.lm.cfg.dataset.segment_duration:  # type: ignore
-    #     raise gr.Error("MusicGen currently supports durations of up to 30 seconds!")
     MODEL.set_generation_params(
         use_sampling=True,
         top_k=params["topk"],
@@ -215,7 +213,9 @@ def generate(params: MusicGenGeneration, melody_in: Optional[Tuple[int, np.ndarr
             outputs_diffusion = mbd.tokens_to_wav(tokens)
             if isinstance(MODEL.compression_model, InterleaveStereoCompressionModel):
                 assert outputs_diffusion.shape[1] == 1  # output is mono
-                outputs_diffusion = rearrange(outputs_diffusion, '(s b) c t -> b (s c) t', s=2)
+                outputs_diffusion = rearrange(
+                    outputs_diffusion, "(s b) c t -> b (s c) t", s=2
+                )
             output = outputs_diffusion.detach().cpu().numpy().squeeze()
         else:
             print("NOTICE: Multi-band diffusion is not supported for AudioGen")
@@ -241,24 +241,7 @@ def generate(params: MusicGenGeneration, melody_in: Optional[Tuple[int, np.ndarr
 
 
 def generation_tab_musicgen():
-    with gr.Tab("MusicGen + AudioGen") as tab:
-        musicgen_atom = gr.JSON(
-            # visible=True,
-            visible=False,
-            value={
-                "text": "",
-                "melody": None,
-                "model": "melody",
-                "duration": 10,
-                "topk": 250,
-                "topp": 0,
-                "temperature": 1.0,
-                "cfg_coef": 3.0,
-                "seed": -1,
-                "use_multi_band_diffusion": False,
-            },
-        )
-        # musicgen_atom.render()
+    with gr.Tab("MusicGen + AudioGen"):
         gr.Markdown(f"""Audiocraft version: {AUDIOCRAFT_VERSION}""")
         with gr.Row(equal_height=False):
             with gr.Column():
@@ -303,7 +286,6 @@ def generation_tab_musicgen():
                         maximum=1.5,
                         value=0.0,
                         label="Top-p",
-                        interactive=True,
                         step=0.05,
                     )
                     temperature = gr.Slider(
@@ -311,7 +293,6 @@ def generation_tab_musicgen():
                         maximum=1.5,
                         value=1.0,
                         label="Temperature",
-                        interactive=True,
                         step=0.05,
                     )
                     cfg_coef = gr.Slider(
@@ -319,7 +300,6 @@ def generation_tab_musicgen():
                         maximum=10.0,
                         value=3.0,
                         label="Classifier Free Guidance",
-                        interactive=True,
                         step=0.1,
                     )
                 use_multi_band_diffusion = gr.Checkbox(
@@ -337,10 +317,7 @@ def generation_tab_musicgen():
             )
             image = gr.Image(label="Waveform", shape=(None, 100), elem_classes="tts-image")  # type: ignore
             with gr.Row():
-                history_bundle_name_data = gr.Textbox(
-                    visible=False,
-                )
-                send_to_demucs_button = gr.Button("Send to Demucs", visible=True)
+                history_bundle_name_data = gr.Textbox(visible=False)
                 save_button = gr.Button("Save to favorites", visible=True)
                 melody_button = gr.Button("Use as melody", visible=True)
             save_button.click(
@@ -355,16 +332,6 @@ def generation_tab_musicgen():
                 outputs=[melody],
             )
 
-            send_to_demucs_button.click(
-                **Joutai.singleton.send_to_demucs(
-                    inputs=[output],
-                )
-            ).then(
-                **Joutai.singleton.switch_to_tab(
-                    tab="demucs",
-                )
-            )
-
     inputs = [
         text,
         melody,
@@ -377,26 +344,6 @@ def generation_tab_musicgen():
         seed,
         use_multi_band_diffusion,
     ]
-
-    def update_components(x):
-        return {
-            text: x["text"],
-            # melody: None,  # due to JSON serialization limitations
-            model: x["model"],
-            duration: x["duration"],
-            topk: x["topk"],
-            topp: x["topp"],
-            temperature: x["temperature"],
-            cfg_coef: x["cfg_coef"],
-            seed: x["seed"],
-            use_multi_band_diffusion: x["use_multi_band_diffusion"],
-        }
-
-    musicgen_atom.change(
-        fn=update_components,
-        inputs=musicgen_atom,
-        outputs=inputs,
-    )
 
     def update_json(
         text,
@@ -435,17 +382,25 @@ def generation_tab_musicgen():
     )
 
     submit.click(
-        fn=update_json,
         inputs=inputs,
-        outputs=[musicgen_atom],
-    ).then(
-        fn=generate,
-        inputs=[musicgen_atom, melody],
+        fn=lambda text, melody, model, duration, topk, topp, temperature, cfg_coef, seed, use_multi_band_diffusion: generate(
+            params=update_json(
+                text,
+                melody,
+                model,
+                duration,
+                topk,
+                topp,
+                temperature,
+                cfg_coef,
+                seed,
+                use_multi_band_diffusion,
+            ),  # type: ignore
+            melody_in=melody,
+        ),
         outputs=[output, history_bundle_name_data, image, seed_cache, result_json],
         api_name="musicgen",
     )
-
-    return tab, musicgen_atom
 
 
 if __name__ == "__main__":
@@ -454,20 +409,20 @@ if __name__ == "__main__":
 
     demo.launch()
 
-    from src.musicgen.musicgen_tab import generate, MusicGenGeneration
+# from src.musicgen.musicgen_tab import generate, MusicGenGeneration
 
-    generate(
-        params=MusicGenGeneration(
-            model="facebook/musicgen-small",
-            text="I am a robot",
-            cfg_coef=3.0,
-            duration=10,
-            melody=None,
-            seed=0,
-            temperature=1.0,
-            topk=250,
-            topp=0.0,
-            use_multi_band_diffusion=False,
-        ),
-        melody_in=None,
-    )
+# generate(
+#     params=MusicGenGeneration(
+#         model="facebook/musicgen-small",
+#         text="I am a robot",
+#         cfg_coef=3.0,
+#         duration=10,
+#         melody=None,
+#         seed=0,
+#         temperature=1.0,
+#         topk=250,
+#         topp=0.0,
+#         use_multi_band_diffusion=False,
+#     ),
+#     melody_in=None,
+# )
