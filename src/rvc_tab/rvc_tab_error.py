cfa279725131a41467d08b25ecb85ec1e30dff30
Roberts Slisans <rsxdalv@gmail.com>
2024-07-19 18:09:24 +0300

    extract generic_error_tab_advanced

--- src/rvc_tab/rvc_tab_error.py
+++ src/rvc_tab/rvc_tab_error.py
@@ -1,25 +1,10 @@
 import gradio as gr
 
-from src.utils.pip_install import pip_install_wrapper
-
-
-def generic_error_tab(e: Exception, name="RVC", requirements="-r requirements_rvc.txt"):
-    with gr.Tab(name + " (!)"):
-        gr.Markdown(f"Failed to load {name} demo")
-        gr.Markdown(f"Error: {e}")
-        gr.Markdown(f"Please install the {requirements} file")
-        gr.Markdown("Please check the console for more information")
-        install_btn = gr.Button(f"Install {name}")
-        gr.Markdown("Installation console:")
-        console_text = gr.HTML()
-        install_btn.click(
-            pip_install_wrapper(requirements, name),
-            outputs=[console_text],
-        )
+from src.utils.generic_error_tab_advanced import generic_error_tab_advanced
 
 
 def rvc_tab_error(e: Exception, name="RVC"):
-    generic_error_tab(e, name=name, requirements="-r requirements_rvc.txt")
+    generic_error_tab_advanced(e, name=name, requirements="-r requirements_rvc.txt")
 
 
 if __name__ == "__main__":
