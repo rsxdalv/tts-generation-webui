da99359e5a10101dd4b2cb4d6d059dd949d37544
Roberts Slisans <rsxdalv@gmail.com>
2024-07-31 21:50:31 +0300

    fix diagnostic.sh

--- installer_scripts/diagnostic.sh
+++ installer_scripts/diagnostic.sh
@@ -4,6 +4,6 @@ export MICROMAMBA_EXE="./installer_files/mamba/micromamba"
 
 echo "Generating diagnostic..."
 
-$MICROMAMBA_EXE run -p ./installer_files/env ./diagnostic_inner.sh
+$MICROMAMBA_EXE run -p ./installer_files/env ./installer_scripts/diagnostic_inner.sh
 
 echo "Diagnostic generated."
