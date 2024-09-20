e4e8381e20b6044555b4f03b68eb20b28c95b6af
Roberts Slisans <rsxdalv@gmail.com>
2024-09-21 00:44:39 +0300

    improve layout

--- react-ui/src/components/StableAudioInputs.tsx
+++ react-ui/src/components/StableAudioInputs.tsx
@@ -38,70 +38,85 @@ export const StableAudioInputs: React.FC<StableAudioInputsProps> = ({
   setStableAudioParams,
 }) => {
   return (
-    <div className="grid grid-cols-1 gap-4">
-      <PromptTextArea
-        params={stableAudioParams}
-        handleChange={handleChange}
-        label="Text"
-        name="text"
-      />
-      <PromptTextArea
-        params={stableAudioParams}
-        handleChange={handleChange}
-        label="Negative Prompt"
-        name="negative_prompt"
-      />
-      <ParameterSlider
-        params={stableAudioParams}
-        onChange={handleChange}
-        label="Seconds Start"
-        name="seconds_start"
-        min="0"
-        max="512"
-        step="1"
-      />
-      <ParameterSlider
-        params={stableAudioParams}
-        onChange={handleChange}
-        label="Seconds Total"
-        name="seconds_total"
-        min="0"
-        max="512"
-        step="1"
-      />
-      <ParameterSlider
-        params={stableAudioParams}
-        onChange={handleChange}
-        label="CFG Scale"
-        name="cfg_scale"
-        min="0"
-        max="50"
-        step="0.01"
-      />
-      <ParameterSlider
-        params={stableAudioParams}
-        onChange={handleChange}
-        label="Steps"
-        name="steps"
-        min="0"
-        max="500"
-        step="1"
-      />
-      <ParameterSlider
-        params={stableAudioParams}
-        onChange={handleChange}
-        label="Preview Every"
-        name="preview_every"
-        min="0"
-        max="100"
-        step="1"
-      />
-      <StableAudioModels
-        params={stableAudioParams}
-        onChange={handleChange}
-        label="Model"
-        name="model_name"
-      />
+    <div className="grid grid-cols-2 gap-4">
+      <div className="flex flex-col gap-2">
+        <PromptTextArea
+          params={stableAudioParams}
+          handleChange={handleChange}
+          label="Text"
+          name="text"
+        />
+        <PromptTextArea
+          params={stableAudioParams}
+          handleChange={handleChange}
+          label="Negative Prompt"
+          name="negative_prompt"
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-2">
+        <div className="flex flex-col gap-2 cell">
+          <ParameterSlider
+            params={stableAudioParams}
+            onChange={handleChange}
+            label="Seconds Start"
+            name="seconds_start"
+            min="0"
+            max="512"
+            step="1"
+          />
+          <ParameterSlider
+            params={stableAudioParams}
+            onChange={handleChange}
+            label="Seconds Total"
+            name="seconds_total"
+            min="0"
+            max="512"
+            step="1"
+          />
+        </div>
+        <div className="flex gap-2 cell">
+          <ParameterSlider
+            params={stableAudioParams}
+            onChange={handleChange}
+            label="CFG Scale"
+            name="cfg_scale"
+            min="0"
+            max="50"
+            step="0.01"
+            decimals={2}
+            orientation="vertical"
+            className="h-40"
+          />
+          <ParameterSlider
+            params={stableAudioParams}
+            onChange={handleChange}
+            label="Steps"
+            name="steps"
+            min="0"
+            max="500"
+            step="1"
+            orientation="vertical"
+            className="h-40"
+          />
+          <ParameterSlider
+            params={stableAudioParams}
+            onChange={handleChange}
+            label="Preview Every"
+            name="preview_every"
+            min="0"
+            max="100"
+            step="1"
+            orientation="vertical"
+            className="h-40"
+          />
+        </div>
+        <StableAudioModels
+          params={stableAudioParams}
+          onChange={handleChange}
+          label="Model"
+          name="model_name"
+        />
+      </div>
     </div>
   );
 };
