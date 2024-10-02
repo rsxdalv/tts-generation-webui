35ec112b5cf8642db11753761d233e46a0c8060a
Roberts Slisans <rsxdalv@gmail.com>
2024-10-03 00:28:24 +0300

    add db version counter

--- installer_scripts/js/applyDatabaseConfig.js
+++ installer_scripts/js/applyDatabaseConfig.js
@@ -6,7 +6,7 @@ const { $sh } = require("./shell.js");
 async function applyDatabaseConfig() {
   const db_conf = {
     // location: "data/postgres",
-    location: resolve(__dirname, "..", "..", "data", "postgres2"),
+    location: resolve(__dirname, "..", "..", "data", "postgres"),
     port: 5432,
     database: "webui",
     username: "postgres",
@@ -61,36 +61,59 @@ async function applyDatabaseConfig() {
     }
   };
 
-  withDatabase(async () => {
-    const createDB = async () => {
-      displayMessage(`Creating database ${db_conf.database}`);
-      // create a database, error if it already exists
-      try {
-        await $sh(`createdb -U ${db_conf.username} ${db_conf.database}`);
-      } catch (error) {
-        displayMessage("Database already exists, skipping...");
-        return;
-      }
-      displayMessage("Successfully created database");
-    };
-    const applyMigrations = async () => {
-      const sql = async (strings) => {
-        // Since we're assuming no interpolated values, we can directly use the string
-        const query = strings[0];
-        const psqlCommand = `psql -U ${db_conf.username} -d ${
-          db_conf.database
-        } -c "${query.replace(/"/g, '\\"').replace(/\n/g, " ")}"`;
-        $sh(psqlCommand);
-      };
+  const db_version = "1";
+  const db_version_file = resolve(__dirname, "..", ".db_version");
 
-      await sql`CREATE TABLE IF NOT EXISTS generations (
-        id SERIAL PRIMARY KEY,
-        metadata JSONB
-      );`;
-    };
-    await createDB();
-    await applyMigrations();
-  });
+  const withDatabaseVersioning = async (func) => {
+    if (fs.existsSync(db_version_file)) {
+      const version = fs.readFileSync(db_version_file, "utf8");
+      if (version === db_version) {
+        displayMessage(
+          `Database is already up to date version=${version}, skipping...`
+        );
+        return;
+      } else {
+        displayMessage(
+          `Database is not up to date, current version=${version}, version=${db_version}, upgrading...`
+        );
+        await func();
+        fs.writeFileSync(db_version_file, db_version);
+      }
+    }
+  };
+
+  await withDatabaseVersioning(() =>
+    withDatabase(async () => {
+      const createDB = async () => {
+        displayMessage(`Creating database ${db_conf.database}`);
+        // create a database, error if it already exists
+        try {
+          await $sh(`createdb -U ${db_conf.username} ${db_conf.database}`);
+        } catch (error) {
+          displayMessage("Database already exists, skipping...");
+          return;
+        }
+        displayMessage("Successfully created database");
+      };
+      const applyMigrations = async () => {
+        const sql = async (strings) => {
+          // Since we're assuming no interpolated values, we can directly use the string
+          const query = strings[0];
+          const psqlCommand = `psql -U ${db_conf.username} -d ${
+            db_conf.database
+          } -c "${query.replace(/"/g, '\\"').replace(/\n/g, " ")}"`;
+          $sh(psqlCommand);
+        };
+
+        await sql`CREATE TABLE IF NOT EXISTS generations (
+      id SERIAL PRIMARY KEY,
+      metadata JSONB
+    );`;
+      };
+      await createDB();
+      await applyMigrations();
+    })
+  );
 }
 
 exports.applyDatabaseConfig = applyDatabaseConfig;
