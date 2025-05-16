f6de89a85116e142a3f3c70d0bf278aa67290572
Roberts Slisans <rsxdalv@gmail.com>
2025-05-17 02:41:51 +0300

    fix download link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -89,7 +89,7 @@ const Navbar = () => {
         </div>
 
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
-          <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
+          <Link href="https://github.com/rsxdalv/tts-generation-webui/archive/refs/heads/main.zip" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
           Download
           </Link>
 
