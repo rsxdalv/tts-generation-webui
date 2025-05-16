35195f4cdeafa839dd2f5d6e9d8e695257d4a535
Roberts Slisans <rsxdalv@gmail.com>
2025-05-17 00:17:54 +0300

    add extras require in setup.py for future installs

--- setup.py
+++ setup.py
@@ -4,6 +4,21 @@ import setuptools
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+# Define optional dependencies
+extras_require = {
+    "cuda": [
+        # "xformers>=0.0.20",
+        # "triton>=2.0.0",
+        # "flash-attn>=2.0.0",
+        "torch==2.6.0+cu126",
+        # "torch==2.7.0+cu126",
+    ],
+    "rocm": [
+        # "torch>=2.0.0",
+        # Add any ROCM specific packages here
+    ],
+}
+
 setuptools.setup(
     name="tts_webui",
     packages=setuptools.find_namespace_packages(),
@@ -19,7 +34,12 @@ setuptools.setup(
     #     "openai",
     # ],
     install_requires=requirements,
+    # install_requires=[],
+    extras_require=extras_require,
     include_package_data=True,
+    # dependency_links=[
+    #     "https://download.pytorch.org/whl/cu126",
+    # ],
     package_data={"": ["*.json"]},
     classifiers=[
         "Programming Language :: Python :: 3",
